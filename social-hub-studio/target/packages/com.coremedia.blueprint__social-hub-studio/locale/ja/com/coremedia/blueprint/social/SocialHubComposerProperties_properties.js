6fa1cef85db6a1b8b576838f3859c9c23377547e
Matthias Faust <matthias.faust@coremedia.com>
2019-10-07 11:16:10 +0200

    Initial 1907.2 commit

--- social-hub-studio/target/packages/com.coremedia.blueprint__social-hub-studio/locale/ja/com/coremedia/blueprint/social/SocialHubComposerProperties_properties.js
+++
@@ -1,8 +0,0 @@
-/**
- * Properties class for ResourceBundle "SocialHubComposerProperties" and Locale "ja".
- */
-Ext.define("com.coremedia.blueprint.social.SocialHubComposerProperties_properties_ja", {
-  override: "com.coremedia.blueprint.social.SocialHubComposerProperties_properties"
-
-}, function() {
-});
