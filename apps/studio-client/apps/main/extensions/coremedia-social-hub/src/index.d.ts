d9c06b30b5395164ee93bf3685baccab88b6d0ec
Eung-Gun Bertelmann <ebertel@coremedia.com>
2021-12-10 12:24:02 +0100

    copy the CKEDITOR class from the cms repository to make CKEDITOR reference compile

---
+++ apps/studio-client/apps/main/extensions/coremedia-social-hub/src/index.d.ts
@@ -0,0 +1,12 @@
+
+declare class CKEDITOR {
+  static TRISTATE_DISABLED: any;
+
+  static TRISTATE_ON: any;
+
+  constructor()
+
+  static on(param: string, param2: import("@jangaroo/runtime").AnyFunction): void;
+
+  static replace(id: string, param2: any): any;
+}
